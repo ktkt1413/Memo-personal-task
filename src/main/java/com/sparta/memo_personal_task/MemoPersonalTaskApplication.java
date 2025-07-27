4646b8552b39ee6ea8491f8c86e2bd38e147ac69
JungDaGyeom <ktkt1413@naver.com>
2025-07-27 23:11:56 +0900

    initial commit

---
+++ src/main/java/com/sparta/memo_personal_task/MemoPersonalTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.memo_personal_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoPersonalTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemoPersonalTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/memo_personal_task/MemoPersonalTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.memo_personal_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemoPersonalTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
